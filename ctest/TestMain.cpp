e81974fb8adeb01a1a754a2543d3d2c39cb6bd70
qqnzhyxxx <tbnzhyxxx@163.com>
2018-06-01 16:45:46 +0800

    Add SpThread Test

--- ctest/TestMain.cpp
+++ ctest/TestMain.cpp
@@ -4,13 +4,42 @@
 
 using namespace std;
 using namespace SpaceDSL;
+class MyTread : public SpThread
+{
+public:
+    MyTread(){}
+    ~MyTread(){}
 
+public:
+
+    void Run() override
+    {
+        int i = 0;
+        do{
+           cout<< "MyThread is Running..."<< i <<endl;
+           ++i;
+           if ( i > 1000)
+           {
+                break;
+           }
+        }while(true);
+
+    }
+};
 
 int main(int argc, char *argv[])
 {
     cout<<"SpaceDSL Test Run!"<<endl;
     try
     {
+        MyTread t;
+        t.Start();
+        t.SetPriority(SpThread::Priority::HighestPriority);
+        cout<<"Before Suspend!"<<endl;
+        //t.Wait();
+        t.Suspend();
+        cout<<"After Suspend!"<<endl;
+        //t.Quit();
         UTCCalTime time(2018,1,4,16,58,11.1);
         IERSService iers;
 
@@ -31,16 +60,18 @@ int main(int argc, char *argv[])
         thirdGravSign.bIsUseMoonGrav = true;
 
         OrbitPredictConfig preConfig;
-        preConfig.Initializer(E_Earth,thirdGravSign,Mjd_TT,E_EGM08Model,20,20,E_1976StdAtmosphere,1.2,2,1.2,5,true,true);
+        preConfig.Initializer(E_Earth,thirdGravSign,Mjd_TT,GravityModel::GravModelType::E_EGM08Model,20,20,
+                              AtmosphereModel::AtmosphereModelType::E_1976StdAtmosphere,1.2,2,1.2,5,true,true);
 
         OrbitPredict orbit;
         Vector3d pos = cart1.Pos(),vel = cart1.Vel();
         cout << "start = " << pos << endl;
         cout << vel << endl;
-        orbit.OrbitStep(preConfig, 10, E_RungeKutta4, mass0, pos, vel);
+        orbit.OrbitStep(preConfig, 10, RungeKutta::E_RungeKutta4, mass0, pos, vel);
         cout << "end = " << pos << endl;
         cout << vel << endl;
-
+        t.Resume();
+        t.Wait();
     }
     catch (SPException &e)
     {
