b7d865ff1bad66b53c442d04071bb6f14f5f79a5
qqnzhyxxx <tbnzhyxxx@163.com>
2018-06-08 17:25:12 +0800

    Test High Precision Orbit Prediction(Without Atmospheric Drag) And Two Body Orbit Prediction

--- ctest/TestMain.cpp
+++ ctest/TestMain.cpp
@@ -14,15 +14,67 @@ public:
 
     void Run() override
     {
-        int i = 0;
-        do{
-           cout<< "MyThread is Running..."<< i <<endl;
-           ++i;
-           if ( i > 1000)
-           {
-                break;
-           }
-        }while(true);
+        UTCCalTime time(2018,1,4,16,58,11.1);
+
+
+        double mass0 = 1000.0;
+        CartState cart0(-5107324.219684929600, -3582177.314118019800, -2477461.707380509900,
+                        4925.070866580500, -3984.883326371030, -4391.339344033090);
+        double Mjd_UTC0 = CalendarTimeToMjd(time);
+
+        double step = 10;
+        double Mjd_UTC = Mjd_UTC0;
+
+        OrbitPredictConfig::ThirdBodyGravitySign thirdGravSign;
+        thirdGravSign.bIsUseSunGrav = true;
+        thirdGravSign.bIsUseMoonGrav = true;
+
+        OrbitPredictConfig preConfig1,preConfig2,preConfig_twoBody;
+        preConfig1.Initializer(Mjd_UTC0, E_Earth, false,
+                              GravityModel::GravModelType::E_EGM08Model,20 , 20, thirdGravSign,
+                              AtmosphereModel::AtmosphereModelType::E_NRLMSISE00Atmosphere, 1.2, 2, 2.2, 20, false, true);
+        preConfig2.Initializer(Mjd_UTC0,E_Earth,false,GravityModel::GravModelType::E_EGM08Model,20,20);
+        preConfig_twoBody.Initializer(Mjd_UTC0,E_Earth,false);
+        TwoBodyOrbitPredict twoBodyPre;
+
+        Vector3d pos = cart0.Pos(), vel = cart0.Vel();
+
+        /*
+        cout << "Time (ModJDate)      x (km)         y (km)        z (km)   vx (km/sec)   vy (km/sec)   vz (km/sec)" <<endl;
+        cout << "---------------    ---------       -------        -------   -----------   -----------  -----------" <<endl;
+        cout.precision(12);
+        cout << Mjd_UTC << "       " <<pos(0)/1000 << "       " <<pos(1)/1000 << "       " <<pos(2)/1000 << "       " <<
+                vel(0)/1000<< "       " <<vel(1)/1000<< "       " <<vel(2)/1000<< "       " <<endl;
+        for (int i = 0; i < DayToSec/step; ++i)
+        {
+            preConfig_twoBody.SetMJD_UTC(Mjd_UTC);
+            twoBodyPre.OrbitStep(preConfig_twoBody,step,RungeKutta::E_RungeKutta4, mass0, pos, vel);
+            Mjd_UTC = Mjd_UTC0 + (i+1) * step/DayToSec;
+            //cout << Mjd_UTC << "       " <<pos(0)/1000 << "       " <<pos(1)/1000 << "       " <<pos(2)/1000 << "       " <<
+            //       vel(0)/1000<< "       " <<vel(1)/1000<< "       " <<vel(2)/1000<< "       " <<endl;
+        }
+        cout << Mjd_UTC << "       " <<pos(0)/1000 << "       " <<pos(1)/1000 << "       " <<pos(2)/1000 << "       " <<
+                vel(0)/1000<< "       " <<vel(1)/1000<< "       " <<vel(2)/1000<< "       " <<endl;
+
+        */
+        OrbitPredict orbit;
+
+        cout << "Time (ModJDate)      x (km)         y (km)        z (km)   vx (km/sec)   vy (km/sec)   vz (km/sec)" <<endl;
+        cout << "---------------    ---------       -------        -------   -----------   -----------  -----------" <<endl;
+        cout.precision(15);
+        cout << Mjd_UTC << "       " <<pos(0)/1000 << "       " <<pos(1)/1000 << "       " <<pos(2)/1000 << "       " <<
+                vel(0)/1000<< "       " <<vel(1)/1000<< "       " <<vel(2)/1000<< "       " <<endl;
+        for (int i = 0; i < DayToSec/step; ++i)
+        {
+            preConfig1.SetMJD_UTC(Mjd_UTC);
+            orbit.OrbitStep(preConfig1, step, RungeKutta::E_RungeKutta4, mass0, pos, vel);
+            Mjd_UTC = Mjd_UTC0 + (i+1) * step/DayToSec;
+            //cout << Mjd_UTC << "       " <<pos(0)/1000 << "       " <<pos(1)/1000 << "       " <<pos(2)/1000 << "       " <<
+            //       vel(0)/1000<< "       " <<vel(1)/1000<< "       " <<vel(2)/1000<< "       " <<endl;
+        }
+        cout << Mjd_UTC << "       " <<pos(0)/1000 << "       " <<pos(1)/1000 << "       " <<pos(2)/1000 << "       " <<
+                vel(0)/1000<< "       " <<vel(1)/1000<< "       " <<vel(2)/1000<< "       " <<endl;
+
 
     }
 };
@@ -36,40 +88,8 @@ int main(int argc, char *argv[])
         t.Start();
         t.SetPriority(SpThread::Priority::HighestPriority);
         cout<<"Before Suspend!"<<endl;
-        //t.Wait();
         t.Suspend();
         cout<<"After Suspend!"<<endl;
-        //t.Quit();
-        UTCCalTime time(2018,1,4,16,58,11.1);
-        IERSService iers;
-
-        double mass0 = 10000.0;
-        CartState cart1(-5107324.219684929600, -3582177.314118019800, -2477461.707380509900,
-                        4925.070866580500, -3984.883326371030, -4391.339344033090);
-        double Mjd_UTC = CalendarTimeToMjd(time);
-        double y_pole = iers.GetValue(Mjd_UTC, "y_pole");
-        char *timeStr = NULL;
-        time.ToCharArray(timeStr);
-
-        cout << "y_pole at " << timeStr <<" = "<< y_pole << "\"" <<endl;
-        double Mjd_TT = CalendarTimeToMjd(time);
-        double Mjd_UT1 = Mjd_TT;
-
-        ThirdBodyGravitySign thirdGravSign;
-        thirdGravSign.bIsUseSunGrav = true;
-        thirdGravSign.bIsUseMoonGrav = true;
-
-        OrbitPredictConfig preConfig;
-        preConfig.Initializer(E_Earth,thirdGravSign,Mjd_TT,GravityModel::GravModelType::E_EGM08Model,20,20,
-                              AtmosphereModel::AtmosphereModelType::E_1976StdAtmosphere,1.2,2,1.2,5,true,true);
-
-        OrbitPredict orbit;
-        Vector3d pos = cart1.Pos(),vel = cart1.Vel();
-        cout << "start = " << pos << endl;
-        cout << vel << endl;
-        orbit.OrbitStep(preConfig, 10, RungeKutta::E_RungeKutta4, mass0, pos, vel);
-        cout << "end = " << pos << endl;
-        cout << vel << endl;
         t.Resume();
         t.Wait();
     }
