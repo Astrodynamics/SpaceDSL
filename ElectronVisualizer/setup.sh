ffaabb189f6e30570fabad783f573c5e68602830
xiaogongwei <xiaogongwei10@gmail.com>
2018-11-01 09:49:09 +0800

    add install shell

---
+++ ElectronVisualizer/setup.sh
@@ -0,0 +1,13 @@
+
+## install npm first.(Tested in Ubuntu OS)
+sudo apt-get install npm
+
+## Install dependencies
+npm install --save-dev electron
+npm install --save-dev cesium
+
+## Run the app
+npm start
+
+## Select file you want visualize ".czml" file
+
