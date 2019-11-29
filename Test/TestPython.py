b90e240d5de85f3593769b8c131f3cd44956ff74
qqnzhyxxx <tbnzhyxxx@163.com>
2019-11-29 13:45:43 +0800

    Fix Test Data

--- Test/TestPython.py
+++ Test/TestPython.py
@@ -15,8 +15,8 @@ def main():
 	vehicle2_mass = 1500.0
 	# Mission Start
 	test_mission = Mission()
-	test_mission.InsertSpaceVehicle('The First Vehicle', initial_time, vehicle1_cart0, vehicle1_mass, 2.2, 10, 1.0, 10)
-	test_mission.InsertSpaceVehicle('The Second Vehicle', initial_time, vehicle2_cart0, vehicle2_mass, 2.2, 20, 1.0, 20)
+	test_mission.InsertSpaceVehicle('The First Vehicle', initial_time, vehicle1_cart0, vehicle1_mass, 2.2, 3, 1.0, 3)
+	test_mission.InsertSpaceVehicle('The Second Vehicle', initial_time, vehicle2_cart0, vehicle2_mass, 2.2, 6, 1.0, 6)
 	test_mission.InsertFacility('Facility1', -75.5966 * DegToRad(), 40.0386 * DegToRad(), 0, 10 * DegToRad())
 	thirdGravSign = ThirdBodyGravitySign()
 	thirdGravSign.bIsUseSunGrav = True
